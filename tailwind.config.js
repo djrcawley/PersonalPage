481622cd864ca3bf566365997e4813df9eee2220
user1243244132423 <118788567+user1243244132423@users.noreply.github.com>
2023-07-03 19:59:31 -0500

    Vite Build to Server

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: 
+    ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {

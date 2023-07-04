56dad15cc09c3eca7e3db0c5c8f74a9ad37d0e05
user1243244132423 <118788567+user1243244132423@users.noreply.github.com>
2023-07-03 21:12:09 -0500

    Update Font

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
       <main className="flex flex-col items-center justify-center min-h-screen en bg-shadow-blue">
-        <span className='text-white text-5xl font-roboto'>Dennis Cawley</span>
+        <span className='text-white text-5xl font-roboto font-semibold'>Dennis Cawley</span>
       </main>
     
   );

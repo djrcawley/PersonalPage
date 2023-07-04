c94b5781b536abd319b348141501d3428698e2d8
user1243244132423 <118788567+user1243244132423@users.noreply.github.com>
2023-07-04 17:36:30 -0500

    Media Respponsive Sizing Icon Sizing for Web&Mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { BsLinkedin, BsInstagram, BsGithub } from 'react-icons/bs';
+import { AiOutlineInfoCircle } from "react-icons/ai"
 
 function App() {
   return (
       <main className="flex flex-col items-center justify-center h-screen bg-shadow-blue">
-        <div className='flex items-center h-screen'>
+        <div className='flex flex-col items-center justify-center flex-1'>
           <span className="text-white text-5xl font-roboto font-semibold">Dennis Cawley</span>
         </div>
-        <div className="flex flex-row justify-center w-screen h-12 ">
-          <BsGithub size="25" className='mx-16'/>
-          <BsLinkedin  size="25" className='mx-16'/>
-          <BsInstagram  size="25" className='mx-16'/>
+        <div className="flex flex-row items-center justify-center w-screen h-20">
+          <BsGithub className='text-white mx-12 md:mx-16 text-2xl md:text-3xl'/>
+          <BsLinkedin className='text-white mx-12 md:mx-16 text-2xl md:text-3xl'/>
+          <BsInstagram className='text-white mx-12 md:mx-16 text-2xl md:text-3xl'/>
         </div>
       </main>
     

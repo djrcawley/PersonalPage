0e7f69754eae8f71e9a5f899465a68e20e41c318
user1243244132423 <118788567+user1243244132423@users.noreply.github.com>
2023-07-04 10:20:19 -0500

    Add Footer Icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { BsLinkedin, BsInstagram, BsGithub } from 'react-icons/bs';
+
 function App() {
   return (
-      <main className="flex flex-col items-center justify-center min-h-screen en bg-shadow-blue">
-        <span className='text-white text-5xl font-roboto font-semibold'>Dennis Cawley</span>
+      <main className="flex flex-col items-center justify-center h-screen bg-shadow-blue">
+        <div className='flex items-center h-screen'>
+          <span className="text-white text-5xl font-roboto font-semibold">Dennis Cawley</span>
+        </div>
+        <div className="flex flex-row justify-center w-screen h-12 ">
+          <BsGithub size="25" className='mx-16'/>
+          <BsLinkedin  size="25" className='mx-16'/>
+          <BsInstagram  size="25" className='mx-16'/>
+        </div>
       </main>
     
   );

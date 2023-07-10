2a02e362e2667cd18677a67a2e577d9917b026f6
user1243244132423 <118788567+user1243244132423@users.noreply.github.com>
2023-07-10 11:47:06 -0500

    Mobile Font Wrap Bug

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <main className="flex flex-col items-center justify-center h-screen bg-shadow-blue">
       <div className='flex flex-col items-center justify-center flex-1'>
-        <span className="text-white text-6xl font-jost font-bold">Dennis Cawley</span>
+        <span className="text-white md:text-6xl text-5xl font-jost font-bold">Dennis Cawley</span>
       </div>
       <div className="flex flex-row items-center justify-center w-screen h-20">
         <a href='https://github.com/djrcawley' target="_blank" rel="noopener noreferrer" className='rounded-full mx-12 md:mx-16 hover:bg-gray-400 w-14 h-14 p-2 flex items-center justify-center'><BsGithub className='text-white text-4xl' /></a>

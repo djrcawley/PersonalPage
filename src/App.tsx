fdfcb3d1a231c7975a689b8d51677cc0d505d40f
user1243244132423 <118788567+user1243244132423@users.noreply.github.com>
2023-07-10 17:49:53 -0500

    Add Surrounding text

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import { BsLinkedin, BsInstagram, BsGithub } from 'react-icons/bs';
 function App() {
   return (
     <main className="flex flex-col items-center justify-center h-screen bg-shadow-blue">
-      <div className='flex flex-col items-center justify-center flex-1'>
-        <span className="text-white md:text-6xl text-5xl font-jost font-bold">Dennis Cawley</span>
+      <div className='flex flex-col justify-center items-center flex-1'>
+        <span className="text-[#D9D9D9] text-2xl font-jost font-bold mb-1">Hello, my name is</span>
+        <div className='h-1 w-64 bg-[#BFBFBF] mb-1.5'></div>
+        <span className="text-white md:text-7xl text-6xl font-jost font-bold mb-1.5">Dennis Cawley</span>
+        <span className="text-[#D9D9D9] text-2xl font-jost font-bold">Software Engineer</span>
       </div>
       <div className="flex flex-row items-center justify-center w-screen h-20">
         <a href='https://github.com/djrcawley' target="_blank" rel="noopener noreferrer" className='rounded-full mx-12 md:mx-16 hover:bg-gray-400 w-14 h-14 p-2 flex items-center justify-center'><BsGithub className='text-white text-4xl' /></a>

0ac2bc16303d091f1213e88ed1c1085d508dbcf3
user1243244132423 <118788567+user1243244132423@users.noreply.github.com>
2023-07-09 20:12:27 -0500

    Add Hover Effect on Socials

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import { BsLinkedin, BsInstagram, BsGithub } from 'react-icons/bs';
-import { AiOutlineInfoCircle } from "react-icons/ai"
 
 function App() {
   return (
-      <main className="flex flex-col items-center justify-center h-screen bg-shadow-blue">
-        <div className='flex flex-col items-center justify-center flex-1'>
-          <span className="text-white text-5xl font-roboto font-semibold">Dennis Cawley</span>
-        </div>
-        <div className="flex flex-row items-center justify-center w-screen h-20">
-          <BsGithub className='text-white mx-12 md:mx-16 text-2xl md:text-3xl'/>
-          <BsLinkedin className='text-white mx-12 md:mx-16 text-2xl md:text-3xl'/>
-          <BsInstagram className='text-white mx-12 md:mx-16 text-2xl md:text-3xl'/>
-        </div>
-      </main>
-    
+    <main className="flex flex-col items-center justify-center h-screen bg-shadow-blue">
+      <div className='flex flex-col items-center justify-center flex-1'>
+        <span className="text-white text-5xl font-roboto font-semibold">Dennis Cawley</span>
+      </div>
+      <div className="flex flex-row items-center justify-center w-screen h-20">
+        <a href='https://github.com/djrcawley' target="_blank" rel="noopener noreferrer" className='rounded-full mx-12 md:mx-16 hover:bg-gray-400 w-14 h-14 p-2 flex items-center justify-center'><BsGithub className='text-white text-4xl' /></a>
+        <a href='https://www.linkedin.com/in/dennis-cawley/' target="_blank" rel="noopener noreferrer" className='rounded-full mx-12 md:mx-16 hover:bg-gray-400 w-14 h-14 p-3 flex items-center justify-center'><BsLinkedin className='text-white text-4xl' /></a>
+        <a href='https://www.instagram.com/djrcawley/' target="_blank" rel="noopener noreferrer" className='rounded-full mx-12 md:mx-16 hover:bg-gray-400 w-14 h-14 p-3 flex items-center justify-center'><BsInstagram className='text-white text-4xl' /></a>
+      </div>
+    </main>
+
   );
 }
 

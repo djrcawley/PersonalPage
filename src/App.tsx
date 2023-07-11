4f1beee8a90ce05a23e41e95c375742ecfce6ad8
user1243244132423 <118788567+user1243244132423@users.noreply.github.com>
2023-07-11 14:26:11 -0500

    Update Link Hover Effect

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,9 @@ function App() {
         <span className="text-[#D9D9D9] text-2xl font-jost font-bold">Software Engineer</span>
       </div>
       <div className="flex flex-row items-center justify-center w-screen h-20">
-        <a href='https://github.com/djrcawley' target="_blank" rel="noopener noreferrer" className='rounded-full mx-12 md:mx-16 hover:bg-gray-400 w-14 h-14 p-2 flex items-center justify-center'><BsGithub className='text-white text-4xl' /></a>
-        <a href='https://www.linkedin.com/in/dennis-cawley/' target="_blank" rel="noopener noreferrer" className='rounded-full mx-12 md:mx-16 hover:bg-gray-400 w-14 h-14 p-3 flex items-center justify-center'><BsLinkedin className='text-white text-4xl' /></a>
-        <a href='https://www.instagram.com/djrcawley/' target="_blank" rel="noopener noreferrer" className='rounded-full mx-12 md:mx-16 hover:bg-gray-400 w-14 h-14 p-3 flex items-center justify-center'><BsInstagram className='text-white text-4xl' /></a>
+        <a href='https://github.com/djrcawley' target="_blank" rel="noopener noreferrer" className='group rounded-full hover:bg-gray-400 mx-12 md:mx-16 w-14 h-14 p-2 flex items-center justify-center'><BsGithub className='text-white group-hover:text-gray-800 text-4xl' /></a>
+        <a href='https://www.linkedin.com/in/dennis-cawley/' target="_blank" rel="noopener noreferrer" className='group rounded-full mx-12 md:mx-16 hover:bg-gray-400 w-14 h-14 p-3 flex items-center justify-center'><BsLinkedin className='text-white group-hover:text-gray-800 text-4xl' /></a>
+        <a href='https://www.instagram.com/djrcawley/' target="_blank" rel="noopener noreferrer" className='group rounded-full mx-12 md:mx-16 hover:bg-gray-400 w-14 h-14 p-3 flex items-center justify-center'><BsInstagram className='text-white group-hover:text-gray-800 text-4xl' /></a>
       </div>
     </main>
 
